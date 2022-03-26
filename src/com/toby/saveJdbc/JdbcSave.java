23dbacf427a5468261cab2a1b1ab30e3c0f52864
toby1a05 <32935616+toby1a05@users.noreply.github.com>
2022-03-26 00:40:17 -0700

    dev

--- src/com/toby/saveJdbc/JdbcSave.java
+++ src/com/toby/saveJdbc/JdbcSave.java
@@ -15,8 +15,10 @@ public class JdbcSave {
 								.addAnnotatedClass(Instructor.class)
 								.addAnnotatedClass(InstructorDetail.class)
 								.buildSessionFactory();
-		
+		Session session3 = factory.getCurrentSession();
+
 		try {
+			/**
 			Session session = factory.getCurrentSession();
 			Instructor ins = new Instructor("Toby","Cheung","toby1a05@gmail.com");
 			InstructorDetail insDetail = new InstructorDetail("Arho","fapping");
@@ -24,20 +26,26 @@ public class JdbcSave {
 			session.beginTransaction();
 			session.save(ins);
 			session.getTransaction().commit();
-			/**                                        **/
+
 			Session session2 = factory.getCurrentSession();
 			session2.beginTransaction();
 			InstructorDetail ins1 = session2.get(InstructorDetail.class,"1");
 			System.out.println(ins1.getInstructor());
 			session2.getTransaction().commit();
-			/**                                        **/
+
 			Session session3 = factory.getCurrentSession();
 			session3.beginTransaction();
 			Instructor ins2 = session3.get(Instructor.class,"1");			
 			System.out.println(ins2.getInstructorDetail());
 			session3.getTransaction().commit();
+			**/
 
+			session3.beginTransaction();
+			Instructor ins2 = session3.get(Instructor.class,"4");			
+			session3.delete(ins2);
+			session3.getTransaction().commit();
 		}finally {
+			session3.close();
 			factory.close();
 		}
 	}
